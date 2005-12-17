d9bb8a16a63ed41c6add33af96e883f1f17bbd1d
Paul Mucur <mudge@mudge.name>
2005-12-17 16:08:27 +0000

    Initial import
    git-svn-id: file:///Volumes/Storage/Archive/repos/python-delicious/trunk@31 feba7eb8-c905-0410-b8ff-9e51836a5b95

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Python-Delicious', version='pre-1.0',
+    description='Python module to access del.icio.us via its API',
+    author='Paul Mucur',
+    author_email='mudge@waferbaby.com',
+    url='http://mudge.name/projects/python-delicious/',
+    py_modules=['delicious'])
